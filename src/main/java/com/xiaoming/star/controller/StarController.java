110ef9766b0a2786cb3633ceb108368ee1545032
liangyi <liangyi@qunhemail.com>
2019-10-28 20:15:02 +0800

    modify

--- src/main/java/com/xiaoming/star/controller/StarController.java
+++ src/main/java/com/xiaoming/star/controller/StarController.java
@@ -1,10 +1,13 @@
 package com.xiaoming.star.controller;
 
 import com.xiaoming.star.service.StarService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
@@ -16,16 +19,33 @@ import java.util.Map;
 @RestController
 public class StarController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(StarController.class);
+
     @Autowired
     private StarService mStarService;
 
     @GetMapping("/test/get")
-    public void test() {
+    public String test() {
         System.out.println("test1 start");
         mStarService.starNum1("test1");
         System.out.println("test2 start");
         mStarService.starNum2("test2");
         System.out.println("end");
+        return "ok";
+    }
+
+    @GetMapping("/test/get2")
+    public String test2(@RequestParam(value = "num") final int num) {
+        final String threadName = Thread.currentThread().getName();
+        try {
+            LOGGER.info("Thread: {} is start, num : {}", threadName, num);
+            Thread.sleep(100);
+        } catch (final InterruptedException e) {
+            LOGGER.error("test2 interrupted exception! num : {}", num, e);
+        }finally {
+            LOGGER.info("Thread: {} is end, num : {}", threadName, num);
+        }
+        return threadName + " : ok";
     }
 
     @PostMapping("/test/post")
@@ -36,5 +56,19 @@ public class StarController {
         System.out.println("test2 start");
         mStarService.starNum2("test2");
         System.out.println("end");
+    }
+
+    @PostMapping("/test/post2")
+    public String testPost2(final int num) {
+        final String threadName = Thread.currentThread().getName();
+        try {
+            LOGGER.info("Thread: {} is start, num : {}", threadName, num);
+            Thread.sleep(100);
+        } catch (final InterruptedException e) {
+            LOGGER.error("test2 interrupted exception! num : {}", num, e);
+        }finally {
+            LOGGER.info("Thread: {} is end, num : {}", threadName, num);
+        }
+        return threadName + " : ok";
     }
 }
